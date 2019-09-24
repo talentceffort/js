4a2c97b5b93ad6aa6991b45287529775ef940859
talentceffort <talentceffort@gmail.com>
2019-09-24 23:02:29 +0900

    closure moudle example

--- closure.js
+++ closure.js
@@ -31,20 +31,25 @@ let add3 = adder(3);
 console.log(add3(15));
 console.log(add3(10));
 
-function getElementByClassName(className) {
-    function recursion(className, parentElemnt) {
-        //parentElement 의 자식 중 className이 있는가?
-        // 있으면 해당 엘리먼트롤 found에 넣는다.
-    
-        // 없으면 해당 엘리먼트의 자식 (children) 찾는다 ->
-        // recursion(className, parentElement[i])
+//클로저 모듈 패턴 : 변수를 스코프 안쪽에 가두어 함수 밖으로 노출 시키지 않는 방법.
+function makeCounter() {
+    let privateCounter = 0;
+
+    return {
+        increment: function() {
+            privateCounter++;
+        },
+        decrement: function() {
+            privateCounter--;
+        },
+        getValue: function() {
+            return privateCounter;
+        }
     }
-    recursion(className, parentElemnt);
 }
 
-//재귀의 장점 : 재귀로 표현하기에 자연스러울 경우 가독성이 좋음.
-//재귀의 단점 : 성능이 매우 안좋음.
-
-//prototype의 의미 => 원형 객체.
-
-console.dir(document.body)
+let counter1 = makeCounter();
+counter1.increment();
+counter1.increment();
+counter1.getValue();
+console.log(counter1)
