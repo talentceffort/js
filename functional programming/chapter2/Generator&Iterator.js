1b387e68188d4878517c9b06c0cd0818b45be918
talentceffort <talentceffort@gmail.com>
2019-11-19 22:38:02 +0900

    Generator basic

--- functional programming/chapter2/Generator&Iterator.js
+++ functional programming/chapter2/Generator&Iterator.js
@@ -19,3 +19,59 @@ log(iter.next())
 log(iter.next())
 
 for (const a of gen()) log(a)
+
+// 자바스크립트에서는 어떤 값이든 iterator 면 순회 가능.
+// Generaotr 를 통해 어떠한 값이든 상태든 순회할 수 있게 만들 수 있음. 함수형 프로그래밍의 관점에서 중요
+// 자바스트립트는 다형성이 굉장히 높다.
+
+// #odds
+function *odds(l) {
+    for (let i = 0; i < l; i++) {
+        if (i % 2) yield i
+    }
+}
+
+// #개선된 odds
+function *odds(l) {
+    for (const a of limit(l, infinity(1))) {
+        if (a % 2) yield a;
+    }
+}
+
+function *infinity(i = 0) {
+    while (true) yield i++
+}
+
+function *limit(l, iter) {
+    for (const a of iter) {
+        yield a;
+        if (a == l) return
+    }
+}
+
+let iter2 = odds(10)
+log(iter2.next())
+log(iter2.next())
+log(iter2.next())
+log(iter2.next())
+log(iter2.next())
+
+let iter3 = infinity();
+log(iter3.next())
+log(iter3.next())
+log(iter3.next())
+log(iter3.next())
+log(iter3.next())
+log(iter3.next())
+log(iter3.next())
+log(iter3.next())
+
+let iter4 = limit(4, [1, 2, 3, 4, 5,6])
+log(iter4.next())
+log(iter4.next())
+log(iter4.next())
+log(iter4.next())
+log(iter4.next())
+log(iter4.next())
+
+for (const a of odds(40)) log(a)
