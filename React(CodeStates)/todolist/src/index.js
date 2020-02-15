eacf004246be27311c43ac4765f950cd2840ef34
talentceffort <talentceffort@gmail.com>
2020-02-15 23:41:19 +0900

    React todolist 시작해보기

--- React(CodeStates)/todolist/src/index.js
+++ React(CodeStates)/todolist/src/index.js
@@ -19,22 +19,52 @@ import ReactDOM from 'react-dom';
 //     </ul>
 // );
 
-const TodoList = (props) => {
-    const onListItemClick = (event) => {
-        console.log('I got clicked');
-    };
+// const TodoList = (props) => {
+//     const onListItemClick = (event) => {
+//         console.log('I got clicked');
+//     };
 
-    // arrow function에 중괄호는 return이 요구되기 때문에 아래와 같이
-    // return 할 값을 작성한 것을 확인하세요!
-    return (
-        <ul>
-            {/* // onClick 등 JSX는 camelCase 문법을 사용합니다. */}
-            <li onClick={onListItemClick}>{props.todos[0]}</li>
-            <li>{props.todos[1]}</li>
-            <li>{props.todos[2]}</li>
-        </ul>
-    );
+//     // arrow function에 중괄호는 return이 요구되기 때문에 아래와 같이
+//     // return 할 값을 작성한 것을 확인하세요!
+//     return (
+//         <ul>
+//             {/* // onClick 등 JSX는 camelCase 문법을 사용합니다. */}
+//             <li onClick={onListItemClick}>{props.todos[0]}</li>
+//             <li>{props.todos[1]}</li>
+//             <li>{props.todos[2]}</li>
+//         </ul>
+//     );
+// }
+// class ToDoListItem은 React의 Component를 부모로 받습니다.
+class TodoList extends React.Component {
+    // constructor 메소드는 constructor에 props를 넘기는데,
+    constructor(props) { 
+      // 부모의 props를 넘기게 됩니다.
+      // constructor안에 super는 필수!
+        super(props);
+      //이로 인해 여기서 this.props는 받은 props가 됩니다.
+
+    }
+    // 모든 클레스는 render메소드가 있어야 합니다. 
+    // 가장 기본의 형태는 이 render 메소드만 있어도 됩니다.
+    render() {
+      // 위에 props를 super(props)로 constructor에 props를 넘겨줬으니, this.props로 부모에게 받은 props를 사용할 수 있게 됩니다.
+        console.log(this.props)
+        return (
+            <React.Fragment>
+                {/* <li>{this.props.todos[0]}</li>
+                <li>{this.props.todos[1]}</li>
+                <li>{this.props.todos[2]}</li> */}
+                <ul>
+                    {this.props.todos.map(v =>
+                        <li key= {v} todos={v} />
+                    )}
+                </ul>
+            </React.Fragment>
+        );
+    }
 }
+
 
 const App = () => (
     <div>
