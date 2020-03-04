727111ca1a390259339963c587c77dfb59b278e9
talentceffort <talentceffort@gmail.com>
2020-03-04 19:27:54 +0900

    hello world 노드

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,14 @@
-const math = require('./math');
-const fs = require('fs');
+const http = require('http');
 
-const { sum } = math;
+const hostname = '127.0.0.1';
+const port = 3000;
 
-console.log(sum(5, 5));
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
 
-const data = fs.readFileSync('./data.txt', 'utf8');
-
-console.log(data);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
