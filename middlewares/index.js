87f7d7e985782cf245c71cc207b6db6ecbd79219
talentceffort <talentceffort@gmail.com>
2020-03-10 23:39:40 +0900

    간단하게 라우팅 만들어보기

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,12 +1,25 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3500;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+
+    console.log(req.url)
+
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, World!\n');
+    } else if (req.url === '/users') {  
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('User list');
+    } else {
+        res.statusCode === 404;
+        res.end('Not Found')
+    }
+    
 });
 
 server.listen(port, hostname, () => {
